eac969e10a1c3a449ab9a5afe550681656bf8559
AhmadAlHajOmar <AhmadAlHajOmar@gmail.com>
2024-10-04 02:44:34 +0300

    first commit

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -10,12 +10,9 @@ class TagResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'body' => $this->body,
-            'user_id' => $this->user_id,
-            'created_at' => $this->created_at->toDateTimeString(),
-            'updated_at' => $this->updated_at->toDateTimeString(),
+            "Tag_id" => $this->id ,
+            "Tag_name" => $this->name
+            
         ];
     }
 }
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -12,10 +12,8 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
-            'created_at' => $this->created_at->toDateTimeString(),
-            'updated_at' => $this->updated_at->toDateTimeString(),
+            'email' => $this->email
+            
         ];
     }
 }
